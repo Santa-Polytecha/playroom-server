a82f20e2eb820031819dcd12441dc3e43e62d36c
LeaChm <chemoul.lea@gmail.com>
2019-11-16 21:44:06 +0100

    :sparkles: Game super class

---
+++ game/game.js
@@ -0,0 +1,53 @@
+const Round = require('../game/round');
+const colors = require('../tools/consoleColors.js');
+
+class Game {
+
+    constructor(roomName, io, gameName) {
+        this.gameName = gameName;
+        this.io = io;
+        this.roomName = roomName;
+        this.isConstrainedByTime = false;
+        this.timeLeft = 3600; //1h
+        this.rounds = [];
+        this.gameId = Date.now();
+
+        if(this.isConstrainedByTime)
+            this.startCountdown();
+
+        this.init();
+    }
+
+    init(){
+        Game.logProcessDone();
+        Game.log("Game created : " + this.gameId);
+        Game.log(`${this.gameName} : Game presentation`);
+    }
+
+    startCountdown() {
+        setInterval(() => {
+            this.io.sockets.in(this.roomName).emit("game-finished");
+        }, this.timeLeft);
+    }
+
+    bindToGameEvents(socket){
+
+    }
+
+    createRound() {
+        console.log('Creating new round at Game: ', this.gameName);
+        const newRound = new Round(this.gameName, this.rounds.length, this.io);
+        return this.rounds.push(newRound);
+    }
+
+    static log(text) {
+        console.log(colors.consoleColors.GameColor, text);
+    }
+
+    static logProcessDone() {
+        console.log(colors.consoleColors.GameDoneColor, " DONE ");
+    }
+
+}
+
+module.exports = Game;
